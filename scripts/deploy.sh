fb124a60579add4ea0fe694cc819671ff348ef7a
Mathieu Langumier <74374019+mlangumier@users.noreply.github.com>
2024-12-02 17:30:16 +0100

    fixed issue in deploy script (#14)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,15 +2,13 @@
 
 # To allow deployment for branches main (production) and develop (preview) only.
 
-env
-
-echo "Vercel Environment: ${ENV_NAME-undefined}"
-echo "Current environment: ${VERCEL_ENV-undefined}"
+echo "Current environment: ${ENV_NAME-undefined}"
+echo "Vercel Environment: ${VERCEL_ENV-undefined}"
 
 [[ "$ENV_NAME" == "production" || "$ENV_NAME" == "development" ]] || {
-  echo ">>> ❌ Deployment cancelled. Missing or invalid environment variables."
+  echo "❌ Deployment cancelled. Missing or invalid environment variables."
   exit 0
 }
 
-echo ">>> ✅ All checks passed. Proceeding with deployment."
+echo "✅ Environment confirmed. Proceeding with deployment."
 exit 1
